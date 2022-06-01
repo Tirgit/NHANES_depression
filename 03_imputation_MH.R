c1902866485d34340addcb1f1638f489c62e0b8d
Tibor V. Varga <tirgit@hotmail.com>
2022-06-01 12:34:55 +0200

    impute

--- 03_imputation_MH.R
+++ 03_imputation_MH.R
@@ -3,30 +3,18 @@ setwd("~/GitHub/NHANES_depression/Data")
 
 # load necessary libraries
 library(mice)
+library(foreign)
 
 # load complete merged data to obtain survey number levels
 # nothing to do with this data, we only need the factor levels for the loop
 cleaned_full_df <- readRDS("cleaned_full_df.rds")
-cleaned_full_df_used <- cleaned_full_df[cleaned_full_df$survey_nr %in% c("2005_2006","2007_2008","2009_2010",
-                                                                         "2011_2012","2013_2014","2015_2016",
-                                                                         "2017_2018"),]
-
-# in the missing_true vector, TRUE means missing values across ALL depression indicators
-missing_true <- rowSums(is.na(cleaned_full_df[,(ncol(cleaned_full_df)-8):ncol(cleaned_full_df)])) == 9
-# in the nonmissing_true vector, TRUE means not completely missing values across depression indicators (so there can be missing values, but not for all indicators)
-nonmissing_true <- !missing_true
-
-# observations that have some depression data
-dep_nonmissing <- cleaned_full_df[nonmissing_true,]
 
 # calculate average missingness in data
-(sum(is.na(dep_nonmissing))/prod(dim(dep_nonmissing)))*100
-# it is 0.6%, we will only impute one copy
+(sum(is.na(cleaned_full_df[cleaned_full_df$phq == "Yes",]))/prod(dim(cleaned_full_df[cleaned_full_df$phq == "Yes",])))*100
+# it is 13.8%, we will impute 15 copies
 
-# load data per survey (loop)
-for (i in levels(cleaned_full_df$survey_nr)) {
-  
-  full_df <- readRDS(paste0("full_",i,".rds"))
+
+  full_df <- cleaned_full_df
   
   # MICE IMPUTATION
   # save variables as vectors that are not needed for imputation
@@ -53,13 +41,15 @@ for (i in levels(cleaned_full_df$survey_nr)) {
     # re-merge ID 
     imputed_df <- as.data.frame(cbind(SEQN, SDMVPSU, SDMVSTRA, imputed))
     
-    filename <- paste0("imputed_",i,"_",j,".rds")
+    filename <- paste0("imputed_",j,".rds")
     
     # save imputed data
     saveRDS(imputed_df, filename)
     
   }
-}
 
+
+  write.dta(imputed_df, "imputed_1.dta")
+  
 
 
